857323120838c375f8eafa2b4d8307efb63f344a
shailendra kumar <shailedrakumar25@gmail.com>
2026-02-13 13:59:57 +0530

    leetcode

---
+++ length of last word.py
@@ -0,0 +1,11 @@
+def lengthOfLastWord(s):
+    length = 0
+    i = len(s) - 1
+    while i >= 0 and s[i] == " ":
+        i -= 1
+    while i >= 0 and s[i] != " ":
+        length += 1
+        i -= 1
+    return length
+print(lengthOfLastWord("my name is shailendra"))
+print(lengthOfLastWord("hello world"))
