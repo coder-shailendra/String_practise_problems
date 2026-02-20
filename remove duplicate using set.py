32b7dc28b4b30326044adc2fa3e9ca48875e726d
shailendra kumar <shailedrakumar25@gmail.com>
2026-02-20 23:14:12 +0530

    remove duplicate by using set

---
+++ remove duplicate using set.py
@@ -0,0 +1,7 @@
+def remove_duplicates(lst):
+    result = []
+    for num in lst:
+        if num not in result:
+            result.append(num)
+    return result
+print(remove_duplicates([1,2,2,3,4,4,5]))
