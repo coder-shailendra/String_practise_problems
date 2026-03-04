ca58f26032f47b535ec95e4764c4b1a8d222133d
shailendra kumar <shailedrakumar25@gmail.com>
2026-03-04 19:14:54 +0530

    leetcode problem

---
+++ a number after a double reversal.py
@@ -0,0 +1,6 @@
+def isSameAfterReversals(num):
+    reversed1 = int(str(num)[::-1])
+    reversed2 = int(str(reversed1)[::-1])
+    return reversed2 == num
+print(isSameAfterReversals(526))  
+print(isSameAfterReversals(1800)) 
