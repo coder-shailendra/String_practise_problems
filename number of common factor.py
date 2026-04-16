f984276fafa878b0adb341ebe27845a49c2f74a9
shailendra kumar <shailedrakumar25@gmail.com>
2026-04-16 20:42:26 +0530

    leetcode problem

---
+++ number of common factor.py
@@ -0,0 +1,8 @@
+def common_factor(a,b):
+    count = 0
+    for i in range(1,min(a,b)+1):
+        if a%i == 0 and b%i == 0:
+            count+=1
+    return count
+print(common_factor(12,6))
+print(common_factor(25,30))
