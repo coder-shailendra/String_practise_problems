84b54be86a12abe3e02e41975b98def2eed1bb0f
shailendra kumar <shailedrakumar25@gmail.com>
2026-04-17 07:00:41 +0530

    leetcpde problem

---
+++ number of good pair.py
@@ -0,0 +1,10 @@
+def good_pairs(nums):
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] == nums[j]:
+                count += 1
+    return count
+print(good_pairs([1,2,3,1,1,3]))
+print(good_pairs([1,1,1,1]))
+print(good_pairs([1,2,3]))
