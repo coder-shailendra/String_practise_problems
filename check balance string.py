bec8716f33a1870beb6ba0620ef6e8ae774fbbe3
shailendra kumar <shailedrakumar25@gmail.com>
2026-04-25 14:40:30 +0530

    leetcode problem

---
+++ check balance string.py
@@ -0,0 +1,12 @@
+def isBalanced(num):
+    even_sum = 0
+    odd_sum = 0
+    for i in range(len(num)):
+        if i % 2 == 0:
+            even_sum += int(num[i])
+        else:
+            odd_sum += int(num[i])
+
+    return even_sum == odd_sum
+print(isBalanced("1234"))   
+print(isBalanced("24123"))  
